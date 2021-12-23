5f765b8001153c5e33f7ae2d401a90c4ea879894
ouler541 <74493223+ouler541@users.noreply.github.com>
2021-12-23 21:40:14 +0700

    main

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 data_trainfull = pd.read_csv('train_full2.csv',delimiter=',')
 data_orders = pd.read_csv('orders.csv')
-data_trainfull.columns = data_trainfull.columns.str.replace(' ', '')
+
 data_trainfull = data_trainfull.reset_index()
 # create a button in the side bar that will move to the next page/radio button choice
-#datasetsatu = data_trainfull[['customer_id', 'gender','location_type','id','OpeningTime','language','vendor_rating','serving_distance','vendor_tag_name','delivery_charge']]
+datasetsatu = data_trainfull[['customer_id', 'gender','location_type','id','OpeningTime','language','vendor_rating','serving_distance','vendor_tag_name','delivery_charge']]
 datasetsatu.rename(columns = {'vendor_rating':'mean_rating'},inplace=True)
 
 #gabungkan dengan id yang sama
